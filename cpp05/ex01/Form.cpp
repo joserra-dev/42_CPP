b8f1b15807e9c459219f5b34c78345478757c63f
joserra70 <joserra70@gmail.com>
2025-09-01 00:04:01 +0200

    añadimos Burocratas

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,5 +1,6 @@
 #include "Form.hpp"
 #include "Bureaucrat.hpp"
+#include <iostream>
 
 //Constructor
 
@@ -16,5 +17,48 @@ Form::Form(const Form& other)
 		isSigned(other.isSigned),
 		gradeToSign(other.gradeToSign),
 		gradeToExecute(other.gradeToExecute) {}
+
+Form& Form::operator=(const Form& other) {
+	if (this != &other) {
+		this->isSigned = other.isSigned;
+		//name, gradeToSign y gradeToExecute son const -> no se reasignan
+	}
+	return *this;
+}
+
+Form::~Form() {}
+
+//Getters
+const std::string& Form::getName() const {return name; }
+bool Form::getIsSigned() const { return isSigned; }
+int Form::getGradeToSign() const { return gradeToSign; }
+int Form::getGradeToExecute() const { return gradeToExecute; }
+
+//Metodo
+void Form::beSigned(const Bureaucrat& b){
+	if(b.getGrade() > gradeToSign)
+		throw GradeTooHighException();
+	isSigned = true;
+}
+
+//Excepciones
+
+const char* Form::GradeTooHighException::what() const throw() {
+	return "Form grade is too high!";
+}
+
+const char* Form::GradeTooLowException::what() const throw() {
+	return "Form grade is too low!";
+}
+
+//Sobrecarga
+
+std::ostream& operator<<(std::ostream& os, const Form& f) {
+	os << "Form \"" <<f.getName()
+		<< "\", signed: " << (f.getIsSigned() ? "yes" : "no")
+		<< ", grade to sign: " << f.getGradeToSign()
+		<< ", grade to execute: " << f.getGradeToExecute();
+	return os;
+}
 
 
